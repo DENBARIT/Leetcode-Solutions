aa788750f669930dac86842f4d0b0cda5e355604
Leul Gebremariam <leulgebremariam1995@gmail.com>
2026-07-20 15:35:36 +0300

    Time: 0 ms (100.00%) | Memory: 57.7 MB (53.66%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -9,27 +9,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
 var reverseList = function(head) {
-    let prev = null;
-    let curr = head;
-    
-    while (curr !== null) {
-        let nextTemp = curr.next;
-        curr.next = prev;       
-        prev = curr;              
-        curr = nextTemp;          
-    }
-    
-    return prev; 
+let current=head;
+let prev=null
+
+while (current!==null){
+    let nextTemp=current.next;
+    current.next=prev;
+    prev=current;
+    current=nextTemp;
+}
+return prev;
 }
